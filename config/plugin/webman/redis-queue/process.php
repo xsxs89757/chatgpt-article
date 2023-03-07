293fd282270fd1a52f9d06d2645cf46b6a572fbe
wanglei <317448423@qq.com>
2023-03-07 15:22:10 +0800

    first commit

--- config/plugin/webman/redis-queue/process.php
+++ config/plugin/webman/redis-queue/process.php
@@ -2,7 +2,7 @@
 return [
     'article'  => [
         'handler'     => Webman\RedisQueue\Process\Consumer::class,
-        'count'       => 8, // 可以设置多进程同时消费
+        'count'       => 5, // 可以设置多进程同时消费
         'constructor' => [
             // 消费者类目录
             'consumer_dir' => app_path() . '/queue/redis/article/'
--- config/plugin/webman/redis-queue/redis.php
+++ config/plugin/webman/redis-queue/redis.php
@@ -7,7 +7,7 @@ return [
             'db' => env('REDIS_DB'),            // 数据库
             'prefix' => '',       // key 前缀
             'max_attempts'  => 5, // 消费失败后，重试次数
-            'retry_seconds' => 60, // 重试间隔，单位秒
+            'retry_seconds' => 10, // 重试间隔，单位秒
             'wait_timeout' => 600
         ]
     ],
