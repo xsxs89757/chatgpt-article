6b7f137f38ff93a6fc74ce6d5665b39843b3d063
wanglei <317448423@qq.com>
2023-05-19 15:46:04 +0800

    修复生成

--- config/plugin/webman/redis-queue/process.php
+++ config/plugin/webman/redis-queue/process.php
@@ -18,7 +18,7 @@ return [
     ],
     'prompt' => [
         'handler'     => Webman\RedisQueue\Process\Consumer::class,
-        'count'       => 10, // 可以设置多进程同时消费
+        'count'       => 100, // 可以设置多进程同时消费
         'constructor' => [
             // 消费者类目录
             'consumer_dir' => app_path() . '/queue/redis/prompt/'
