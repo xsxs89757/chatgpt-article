09bdd5de2b6d1e060b17ed12dafcba8b450d65c3
wanglei <317448423@qq.com>
2023-05-20 10:56:42 +0800

    修复生成

--- app/queue/redis/prompt/Prompt.php
+++ app/queue/redis/prompt/Prompt.php
@@ -5,6 +5,7 @@ namespace app\queue\redis\prompt;
 use app\kernel\help\CgptHttp;
 use App\model\Article as ModelArticle;
 use App\model\Word as ModelWord;
+use support\Log;
 use Webman\RedisQueue\Consumer;
 
 class Prompt implements Consumer
@@ -18,45 +19,51 @@ class Prompt implements Consumer
     // 消费
     public function consume($data)
     {   
-        $resultContent = CgptHttp::getMessage($data['word']);
-        $content = $resultContent['content'];
-        $parent_message_id = $resultContent['parent_message_id'];
-        $resultTags = CgptHttp::getMessage(
-            '根据上面生成的文章生成title并提取keywords、description、tags', 
-            $parent_message_id
-        );
-        $tdk = explode(PHP_EOL, $resultTags['content']);
-        $keyword = $description = $tags = "";
-        foreach($tdk as $value) {
-            if($value){
-                $tdkArr = explode(':', $value);
-                if($tdkArr[0] === 'Title' || $tdkArr[0] === '标题'){
-                    $title = $tdkArr[1];
-                }
-                if($tdkArr[0] === 'Keywords' || $tdkArr[0] === '关键词'){
-                    $keyword = $tdkArr[1];
-                }
-                if($tdkArr[0] === 'Description' || $tdkArr[0] === '描述'){
-                    $description = $tdkArr[1];
-                }
-                if($tdkArr[0] === 'Tags' || $tdkArr[0] === '标签'){
-                    $tags = str_replace('、',',',$tdkArr[1]);
-                    $tags = str_replace('。','',$tags);
-                }
+        try{
+            $resultContent = CgptHttp::getMessage($data['word']);
+            $content = $resultContent['content'];
+            $parent_message_id = $resultContent['parent_message_id'];
+            $resultTags = CgptHttp::getMessage(
+                '根据上面生成的文章生成title并提取keywords、description、tags', 
+                $parent_message_id
+            );
+            $tdk = explode(PHP_EOL, $resultTags['content']);
+            $keyword = $description = $tags = "";
+            foreach($tdk as $value) {
+                if($value){
+                    $tdkArr = explode(':', $value);
+                    if($tdkArr[0] === 'Title' || $tdkArr[0] === '标题'){
+                        $title = $tdkArr[1];
+                    }
+                    if($tdkArr[0] === 'Keywords' || $tdkArr[0] === '关键词'){
+                        $keyword = $tdkArr[1];
+                    }
+                    if($tdkArr[0] === 'Description' || $tdkArr[0] === '描述'){
+                        $description = $tdkArr[1];
+                    }
+                    if($tdkArr[0] === 'Tags' || $tdkArr[0] === '标签'){
+                        $tags = str_replace('、',',',$tdkArr[1]);
+                        $tags = str_replace('。','',$tags);
+                    }
 
+                }
             }
-        }
-        $data = [
-            'title' => $title,
-            'keywords' => $keyword,
-            'description' => $description,
-            'tags' => $tags,
-            'content' => $content,
-            'word' => $data['y_word'],
-            'word_id' => $data['task_id'],
-        ];
+            $data = [
+                'title' => $title,
+                'keywords' => $keyword,
+                'description' => $description,
+                'tags' => $tags,
+                'content' => $content,
+                'word' => $data['y_word'],
+                'word_id' => $data['task_id'],
+            ];
 
-        ModelArticle::store($data);
-        ModelWord::progress($data['word_id']);
+            ModelArticle::store($data);
+            ModelWord::progress($data['word_id']);
+        }catch(\Throwable $e){
+            Log::debug('ERROR: ' . $e->getMessage());
+        }
+
+        
     }
 }
